d3da7550e2c68f9ba4ca6c0cae08e8601d4bc1ad
Kyle Benson <kyleabenson@gmail.com>
2021-11-09 20:48:25 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,93 @@
+# Copyright 2021 Google LLC
+
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+
+#         https://www.apache.org/licenses/LICENSE-2.0
+
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+
+// Configure the Google Cloud provider
+provider "google" {
+  project     = var.gcp_project
+  region      = var.gcp_region
+}
+
+
+////
+resource "google_compute_instance_template" "default" {
+  name        = "appserver-template"
+  description = "This template is used to create app server instances."
+
+  tags = ["foo", "bar"]
+
+  labels = {
+    environment = "dev"
+  }
+
+  instance_description = "description assigned to instances"
+  machine_type         = "e2-medium"
+  can_ip_forward       = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  // Create a new boot disk from an image
+  disk {
+    source_image      = "debian-cloud/debian-9"
+    auto_delete       = true
+    boot              = true
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+     // Include this section to give the VM an external ip address
+   }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+  
+  metadata = {
+    foo = "bar"
+  }
+
+
+}
+
+data "google_compute_image" "my_image" {
+  family  = "debian-9"
+  project = "debian-cloud"
+}
+
+resource "google_compute_disk" "foobar" {
+  name  = "existing-disk"
+  image = data.google_compute_image.my_image.self_link
+  size  = 10
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+}
+
+
+
+resource "google_compute_instance_group_manager" "instance_group_manager" {
+  name               = "instance-group-manager"
+  base_instance_name = "instance-group-manager"
+  zone               = "us-central1-f"
+  target_size        = "3"
+  version {
+    name = "test"
+    instance_template  = google_compute_instance_template.default.id
+  }
+}
---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script installes httpd on the system, then adds the Cloud Ops agent
+sudo apt-get update -y
+sudo apt-get install apache2 -y
+
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install

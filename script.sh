9a72b110724100e51212694b080f6559c5a771eb
Kyle Benson <kyleabenson@gmail.com>
2021-11-15 17:23:30 +0000

    move agent management to ansible

--- script.sh
+++ script.sh
@@ -15,5 +15,3 @@ Metadata: $METADATA
 EOF
 
 sudo systemctl start nginx
-# curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
-# sudo bash add-google-cloud-ops-agent-repo.sh --also-install
